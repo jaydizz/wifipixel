b80072fa0a8e07c9833085bb0b2b5a525e00573f
Johannes Deger <johannes.deger@uni-ulm.de>
2017-10-23 14:39:04 +0200

    removed unused functions.

--- src/NodeMCU.cpp
+++ src/NodeMCU.cpp
@@ -166,14 +166,7 @@ uint32_t Wheel(byte WheelPos) {
     return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c) {
-    for(uint16_t i=0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, c);
-        strip.show();
-        delay(waittime);
-    }
-}
+
 
 void colorwipe() {
 
@@ -206,18 +199,7 @@ void rainbow() {
     }
     strip.show();
 }
-/*
-BLYNK_WRITE(V1)
-{
-  int shift = param.asInt();
-  for (int i = 0; i < strip.numPixels(); i++)
-  {
-    strip.setPixelColor(i, Wheel(shift & 255));
-    // OR: strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + shift) & 255));
-  }
-  strip.show();
-}
-*/
+
 
 void chase() {
     for(int i =0; i < strip.numPixels(); i++) {
